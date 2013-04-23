aa5613a53c6a30ad1d231122488fb3d19766eda8
Peter Wells <peterwells723@yahoo.com>
2013-04-23 08:28:57 -0400

    Removed the .gitignore file and added work done yesterday to repository.

---
+++ ruby_pwells2/bakeoff_webapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BakeoffWebapp::Application.config.secret_token = '58d841e0bc8e67f241f2069556bfc342364ce9f6731880b4f9749844f10daf678cccc5dd817b2b7bab7cb206ec8c8d12b7a31353dad0b742d4b94da6a51cdcc8'
